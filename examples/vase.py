f123396ff13e0d747fb59cf1cc4f9e9f591f41d8
Roipoussiere <roipoussiere@protonmail.com>
2022-09-12 03:55:24 +0200

    update the vase example with the new import api

--- examples/vase.py
+++ examples/vase.py
@@ -25,37 +25,39 @@ license:
     See the License for the specific language governing permissions and
     limitations under the License.
 """
-from cadquery import Vector
-from build123d import *
 
-with BuildPart() as vase:
-    with BuildSketch() as profile:
-        with BuildLine() as outline:
-            l1 = Line((0, 0), (12, 0))
-            l2 = RadiusArc(l1 @ 1, (15, 20), 50)
-            l3 = Spline(l2 @ 1, (22, 40), (20, 50), tangents=(l2 % 1, (-0.75, 1)))
-            l4 = RadiusArc(l3 @ 1, l3 @ 1 + Vector(0, 5), 5)
-            l5 = Spline(
+from cadquery import Vector
+from build123d import common as bc, generic as bg, part as bp, sketch as bs, line as bl
+
+
+with bp.BuildPart() as vase:
+    with bs.BuildSketch() as profile:
+        with bl.BuildLine() as outline:
+            l1 = bl.Line((0, 0), (12, 0))
+            l2 = bl.RadiusArc(l1 @ 1, (15, 20), 50)
+            l3 = bl.Spline(l2 @ 1, (22, 40), (20, 50), tangents=(l2 % 1, (-0.75, 1)))
+            l4 = bl.RadiusArc(l3 @ 1, l3 @ 1 + Vector(0, 5), 5)
+            l5 = bl.Spline(
                 l4 @ 1,
                 l4 @ 1 + Vector(2.5, 2.5),
                 l4 @ 1 + Vector(0, 5),
                 tangents=(l4 % 1, (-1, 0)),
             )
-            Polyline(
+            bl.Polyline(
                 l5 @ 1,
                 l5 @ 1 + Vector(0, 1),
                 (0, (l5 @ 1).y + 1),
                 l1 @ 0,
             )
-        BuildFace()
-    Revolve()
-    Shell(vase.faces().filter_by_axis(Axis.Y)[-1], thickness=-1)
+        bs.BuildFace()
+    bp.Revolve()
+    bp.Shell(vase.faces().filter_by_axis(bg.Axis.Y)[-1], thickness=-1)
     top_edges = (
-        vase.edges().filter_by_position(Axis.Y, 60, 62).filter_by_type(Type.CIRCLE)
+        vase.edges().filter_by_position(bg.Axis.Y, 60, 62).filter_by_type(bc.Type.CIRCLE)
     )
     # debug(top_edges)
-    Fillet(*top_edges, radius=0.25)
-    Fillet(vase.edges().sort_by(SortBy.Y)[0], radius=0.5)
+    bg.Fillet(*top_edges, radius=0.25)
+    bg.Fillet(vase.edges().sort_by(bc.SortBy.Y)[0], radius=0.5)
 
 
 if "show_object" in locals():
