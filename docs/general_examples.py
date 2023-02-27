37f088063b1416377622c1c1c7419cb2d93e3152
jdegenstein <jdegenstein@users.noreply.github.com>
2023-02-27 14:57:41 -0600

    stop_angle -> angular_range in Ex13 PolarLocations

--- docs/general_examples.py
+++ docs/general_examples.py
@@ -310,7 +310,7 @@ with BuildPart() as ex13:
     with Workplanes(ex13.faces().sort_by(Axis.Z)[-1]):
         with PolarLocations(radius=a, count=4):
             CounterSinkHole(radius=b, counter_sink_radius=2 * b)
-        with PolarLocations(radius=a, count=4, start_angle=45, stop_angle=360 + 45):
+        with PolarLocations(radius=a, count=4, start_angle=45, angular_range=360):
             CounterBoreHole(radius=b, counter_bore_radius=2 * b, counter_bore_depth=b)
 # [Ex. 13]
 
