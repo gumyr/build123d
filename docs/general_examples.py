e9d244fc3a7e3d19f187798ac44dacbf7e85d1b0
jdegenstein <jdegenstein@users.noreply.github.com>
2023-04-07 16:50:29 -0500

    intro ex24 remove unnecessary parens

--- docs/general_examples.py
+++ docs/general_examples.py
@@ -553,7 +553,7 @@ length, width, thickness = 80.0, 60.0, 10.0
 
 with BuildPart() as ex24:
     Box(length, length, thickness)
-    with BuildSketch((ex24.faces().group_by(Axis.Z)[0])[0]) as ex24_sk:
+    with BuildSketch(ex24.faces().group_by(Axis.Z)[0][0]) as ex24_sk:
         Circle(length / 3)
     with BuildSketch(ex24_sk.faces()[0].offset(length / 2)) as ex24_sk2:
         Rectangle(length / 6, width / 6)
