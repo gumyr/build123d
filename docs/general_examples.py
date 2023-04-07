49064c12c6cce6580678ca7407bbae0c26898c5c
jdegenstein <jdegenstein@users.noreply.github.com>
2023-04-07 14:01:43 -0500

    remove ex22 unnecessary parens

--- docs/general_examples.py
+++ docs/general_examples.py
@@ -503,7 +503,7 @@ length, width, thickness = 80.0, 60.0, 10.0
 
 with BuildPart() as ex22:
     Box(length, width, thickness)
-    pln = Plane((ex22.faces().group_by(Axis.Z)[0])[0]).rotated((0, -50, 0))
+    pln = Plane(ex22.faces().group_by(Axis.Z)[0][0]).rotated((0, -50, 0))
     with BuildSketch(pln) as ex22_sk:
         with GridLocations(length / 4, width / 4, 2, 2):
             Circle(thickness / 4)
