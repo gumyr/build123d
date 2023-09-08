66cef02064abc172a8e849c53c75a2b830edf3dd
Jojain <50221409+Jojain@users.noreply.github.com>
2023-09-08 23:53:09 +0200

    Remove TopLoc code since it cannot be loaded back

--- src/build123d/persistence.py
+++ src/build123d/persistence.py
@@ -2,8 +2,6 @@ import platform
 import tempfile
 from OCP.BinTools import BinTools
 from OCP.TopoDS import TopoDS_Shape, TopoDS_Compound, TopoDS_CompSolid, TopoDS_Solid, TopoDS_Shell, TopoDS_Face, TopoDS_Wire, TopoDS_Edge, TopoDS_Vertex
-from OCP.TopLoc import TopLoc_Location
-from OCP.gp import gp_Trsf
 import io
 import copyreg
 from build123d.topology import downcast
@@ -45,35 +43,9 @@ def deserialize_shape(buffer : bytes):
         BinTools.Read_s(shape, bio)
     return downcast(shape)
 
-def serialize_loc(loc : TopLoc_Location):
-    """
-    Serialize a OCP TopLoc_Location, this method can be used to provide a custom serialization algo for pickle
-    """
-    bio = io.BytesIO()
-    loc.Transformation().DumpJson(bio)
-    buffer = bio.getvalue()
-    return buffer
-
-def deserialize_loc(buffer : bytes):
-    """
-    This does the opposite as serialize, it construct a TopLoc_Location from bytes.
-    """
-    if buffer is None:
-        return None
-    t = gp_Trsf()
-    gp_Trsf()
-    sio = buffer.decode()
-    t.InitFromJson(sio,0)
-    loc = TopLoc_Location(t)
-    
-    return loc
-
 def reduce_shape(shape : TopoDS_Shape):
     """Special function used by pickle to serialize or deserialize OCP Shapes objects"""
     return (deserialize_shape, (serialize_shape(shape),))
-
-def reduce_loc(loc: TopLoc_Location):
-    return (deserialize_loc, (serialize_loc(loc),))
 
 def modify_copyreg():
     """Modify the copyreg so that pickle knows what to look for when it tries to pickle an OCP Shape"""
@@ -86,12 +58,3 @@ def modify_copyreg():
     copyreg.pickle(TopoDS_Wire, reduce_shape)
     copyreg.pickle(TopoDS_Edge, reduce_shape)
     copyreg.pickle(TopoDS_Vertex, reduce_shape)
-    copyreg.pickle(TopLoc_Location, reduce_loc)
-
-if __name__ == "__main__":
-    from build123d import *
-    import pickle
-    a = Box(1,2,3)
-    c = pickle.dumps(a)
-    b = pickle.loads(c)
-    b
