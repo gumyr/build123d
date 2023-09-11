775656f6b19cd1011564aea805b52904f44c472e
Romain FERRU <romain.ferru@gmail.com>
2023-09-11 23:45:46 +0200

    Added serialization of TopLoc_Location and tests
    Finally I managed (with the help of Bernhard) to write a serializer for locs, that way one can use pickle directly on BaseObjects

--- src/build123d/persistence.py
+++ src/build123d/persistence.py
@@ -1,31 +1,37 @@
-import platform
-import tempfile
 from OCP.BinTools import BinTools
-from OCP.TopoDS import TopoDS_Shape, TopoDS_Compound, TopoDS_CompSolid, TopoDS_Solid, TopoDS_Shell, TopoDS_Face, TopoDS_Wire, TopoDS_Edge, TopoDS_Vertex
+from OCP.TopoDS import (
+    TopoDS_Shape,
+    TopoDS_Compound,
+    TopoDS_CompSolid,
+    TopoDS_Solid,
+    TopoDS_Shell,
+    TopoDS_Face,
+    TopoDS_Wire,
+    TopoDS_Edge,
+    TopoDS_Vertex,
+)
+from OCP.TopLoc import TopLoc_Location
+from OCP.gp import gp_Trsf, gp_Quaternion, gp_Vec
 import io
 import copyreg
 from build123d.topology import downcast
+import struct
 
-def serialize_shape(shape : TopoDS_Shape):
+
+def serialize_shape(shape: TopoDS_Shape):
     """
     Serialize a OCP shape, this method can be used to provide a custom serialization algo for pickle
     """
     if shape is None:
         return None
 
-    if platform.system() == "Darwin":
-        with tempfile.NamedTemporaryFile() as tf:
-            BinTools.Write_s(shape, tf.name)
-            with open(tf.name, "rb") as fd:
-                buffer = fd.read()
-    else:
-        bio = io.BytesIO()
-        BinTools.Write_s(shape, bio)
-        buffer = bio.getvalue()
+    bio = io.BytesIO()
+    BinTools.Write_s(shape, bio)
+    buffer = bio.getvalue()
     return buffer
 
 
-def deserialize_shape(buffer : bytes):
+def deserialize_shape(buffer: bytes):
     """
     This does the opposite as serialize, it construct a TopoDS_Shape from bytes.
     """
@@ -33,19 +39,75 @@ def deserialize_shape(buffer : bytes):
         return None
 
     shape = TopoDS_Shape()
-    if platform.system() == "Darwin":
-        with tempfile.NamedTemporaryFile() as tf:
-            with open(tf.name, "wb") as fd:
-                fd.write(buffer)
-            BinTools.Read_s(shape, tf.name)
-    else:
-        bio = io.BytesIO(buffer)
-        BinTools.Read_s(shape, bio)
+    bio = io.BytesIO(buffer)
+    BinTools.Read_s(shape, bio)
     return downcast(shape)
 
-def reduce_shape(shape : TopoDS_Shape):
+
+def serialize_location(location: TopLoc_Location):
+    """
+    Serialize a OCP location, this method can be used to provide a custom serialization algo for pickle
+    """
+    if location is None:
+        return None
+    transfo = location.Transformation()
+    translation = transfo.TranslationPart()
+    rotation = transfo.GetRotation()
+    # convert floats in bytes
+    translation_bytes = bytearray()
+    for i in range(1, 4):
+        translation_bytes.extend(struct.pack("f", translation.Coord(i)))
+    rotation_bytes = bytearray()
+    rotation_bytes.extend(struct.pack("f", rotation.X()))
+    rotation_bytes.extend(struct.pack("f", rotation.Y()))
+    rotation_bytes.extend(struct.pack("f", rotation.Z()))
+    rotation_bytes.extend(struct.pack("f", rotation.W()))
+
+    buffer = bytearray()
+    buffer.extend(translation_bytes)
+    buffer.extend(rotation_bytes)
+    return buffer
+
+
+def deserialize_location(buffer: bytes):
+    """
+    This does the opposite as serialize, it construct a TopLoc_Location from bytes.
+    """
+    if buffer is None:
+        return None
+
+    # Makes 4 bytes chunks (floats are 4 bytes long)
+    chunks = iter(struct.iter_unpack("f", buffer))
+    translation = gp_Vec(
+        next(chunks)[0],
+        next(chunks)[0],
+        next(chunks)[0],
+    )
+
+    # Read the rotation bytes
+    rotation = gp_Quaternion(
+        next(chunks)[0],
+        next(chunks)[0],
+        next(chunks)[0],
+        next(chunks)[0],
+    )
+
+    # Create the TopLoc_Location object
+    transfo = gp_Trsf()
+    transfo.SetTransformation(rotation, translation)
+
+    return TopLoc_Location(transfo)
+
+
+def reduce_shape(shape: TopoDS_Shape):
     """Special function used by pickle to serialize or deserialize OCP Shapes objects"""
     return (deserialize_shape, (serialize_shape(shape),))
+
+
+def reduce_location(location: TopLoc_Location):
+    """Special function used by pickle to serialize or deserialize OCP Location objects"""
+    return (deserialize_location, (serialize_location(location),))
+
 
 def modify_copyreg():
     """Modify the copyreg so that pickle knows what to look for when it tries to pickle an OCP Shape"""
@@ -58,3 +120,4 @@ def modify_copyreg():
     copyreg.pickle(TopoDS_Wire, reduce_shape)
     copyreg.pickle(TopoDS_Edge, reduce_shape)
     copyreg.pickle(TopoDS_Vertex, reduce_shape)
+    copyreg.pickle(TopLoc_Location, reduce_location)
---
+++ tests/test_persistence.py
@@ -0,0 +1,91 @@
+# write me the test for testing the persistence module with unnitest
+
+import unittest
+from build123d.persistence import (
+    serialize_shape,
+    deserialize_shape,
+    serialize_location,
+    deserialize_location,
+)
+from build123d import *
+import pickle
+
+
+class TestPersistence(unittest.TestCase):
+    def test_serialize_shape(self):
+        edge = Edge.make_line((10, 5, 0), (10, 5, 2))
+        wire = Wire.make_circle(10)
+        face = Face.make_rect(10, 5)
+        solid = Solid.make_box(10, 5, 2)
+        compound = Compound.make_compound([edge, wire, face, solid])
+
+        buffer = serialize_shape(edge.wrapped)
+        retrived_edge = deserialize_shape(buffer)
+        self.assertAlmostEqual(edge.length, Edge(retrived_edge).length)
+
+        buffer = serialize_shape(wire.wrapped)
+        retrived_wire = deserialize_shape(buffer)
+        self.assertAlmostEqual(len(wire.edges()), len(Wire(retrived_wire).edges()))
+
+        buffer = serialize_shape(face.wrapped)
+        retrived_face = deserialize_shape(buffer)
+        self.assertAlmostEqual(face.area, Face(retrived_face).area)
+
+        buffer = serialize_shape(solid.wrapped)
+        retrived_solid = deserialize_shape(buffer)
+        self.assertAlmostEqual(solid.volume, Solid(retrived_solid).volume)
+
+        buffer = serialize_shape(compound.wrapped)
+        retrived_comp = deserialize_shape(buffer)
+        self.assertEqual(len(compound.solids()), len(Compound(retrived_comp).solids()))
+
+    def test_serialize_location(self):
+        loc = Location((10, 5, 0), (16, 1, 1)).wrapped
+        transfo = loc.Transformation()
+
+        buffer = serialize_location(loc)
+        retrived_loc = deserialize_location(buffer)
+        retrived_transfo = retrived_loc.Transformation()
+        self.assertAlmostEqual(
+            retrived_transfo.TranslationPart().X(), transfo.TranslationPart().X()
+        )
+        self.assertAlmostEqual(
+            retrived_transfo.TranslationPart().Y(), transfo.TranslationPart().Y()
+        )
+        self.assertAlmostEqual(
+            retrived_transfo.TranslationPart().Z(), transfo.TranslationPart().Z()
+        )
+        self.assertAlmostEqual(
+            retrived_transfo.GetRotation().X(), transfo.GetRotation().X()
+        )
+        self.assertAlmostEqual(
+            retrived_transfo.GetRotation().Y(), transfo.GetRotation().Y()
+        )
+        self.assertAlmostEqual(
+            retrived_transfo.GetRotation().Z(), transfo.GetRotation().Z()
+        )
+        self.assertAlmostEqual(
+            retrived_transfo.GetRotation().W(), transfo.GetRotation().W()
+        )
+
+    def test_serialize_obj(self):
+        # The pickle test tests the serialization of toplevel objects (OCP Shapes and OCP Locs are also tested this way)
+        line = Line((10, 5, 0), (0, 5, 2))
+        face = Circle(50)
+        solid = Box(10, 5, 2)
+
+        dumped_line = pickle.dumps(line)
+        retrived_line = pickle.loads(dumped_line)
+        self.assertAlmostEqual(line.length, retrived_line.length)
+
+        dumped_face = pickle.dumps(face)
+        retrived_face = pickle.loads(dumped_face)
+        self.assertAlmostEqual(face.area, retrived_face.area)
+
+        dumped_solid = pickle.dumps(solid)
+        retrived_solid = pickle.loads(dumped_solid)
+        self.assertAlmostEqual(solid.volume, retrived_solid.volume)
+
+
+if __name__ == "__main__":
+    unittest.main()
