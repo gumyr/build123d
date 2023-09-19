3031c500442408e33149cca11d3feb1d9746e0a3
Romain FERRU <romain.ferru@gmail.com>
2023-09-19 22:33:39 +0200

    Update persistence.py

--- src/build123d/persistence.py
+++ src/build123d/persistence.py
@@ -1,24 +1,55 @@
-from OCP.BinTools import BinTools
-from OCP.TopoDS import (
-    TopoDS_Shape,
-    TopoDS_Compound,
-    TopoDS_CompSolid,
-    TopoDS_Solid,
-    TopoDS_Shell,
-    TopoDS_Face,
-    TopoDS_Wire,
-    TopoDS_Edge,
-    TopoDS_Vertex,
-)
-from OCP.TopLoc import TopLoc_Location
-from OCP.gp import gp_Trsf, gp_Quaternion, gp_Vec
-import io
+"""
+build123d pickle support
+
+name: persistence.py
+by:   Jojain & bernhard-42
+date: September 8th, 2023
+
+desc:
+    This python module enables build123d objects to be pickled.
+
+license:
+
+    Copyright 2023 Jojain & bernhard-42
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+"""
+# pylint has trouble with the OCP imports
+# pylint: disable=no-name-in-module, import-error
+
 import copyreg
-from build123d.topology import downcast
+import io
 import struct
 
+from build123d.topology import downcast
+from OCP.BinTools import BinTools
+from OCP.gp import gp_Quaternion, gp_Trsf, gp_Vec
+from OCP.TopLoc import TopLoc_Location
+from OCP.TopoDS import (
+    TopoDS_Compound,
+    TopoDS_CompSolid,
+    TopoDS_Edge,
+    TopoDS_Face,
+    TopoDS_Shape,
+    TopoDS_Shell,
+    TopoDS_Solid,
+    TopoDS_Vertex,
+    TopoDS_Wire,
+)
 
-def serialize_shape(shape: TopoDS_Shape):
+
+def serialize_shape(shape: TopoDS_Shape) -> bytes:
     """
     Serialize a OCP shape, this method can be used to provide a custom serialization algo for pickle
     """
@@ -31,7 +62,7 @@ def serialize_shape(shape: TopoDS_Shape):
     return buffer
 
 
-def deserialize_shape(buffer: bytes):
+def deserialize_shape(buffer: bytes) -> TopoDS_Shape:
     """
     This does the opposite as serialize, it construct a TopoDS_Shape from bytes.
     """
@@ -44,7 +75,7 @@ def deserialize_shape(buffer: bytes):
     return downcast(shape)
 
 
-def serialize_location(location: TopLoc_Location):
+def serialize_location(location: TopLoc_Location) -> bytes:
     """
     Serialize a OCP location, this method can be used to provide a custom serialization algo for pickle
     """
@@ -69,7 +100,7 @@ def serialize_location(location: TopLoc_Location):
     return buffer
 
 
-def deserialize_location(buffer: bytes):
+def deserialize_location(buffer: bytes) -> TopLoc_Location:
     """
     This does the opposite as serialize, it construct a TopLoc_Location from bytes.
     """
@@ -99,12 +130,12 @@ def deserialize_location(buffer: bytes):
     return TopLoc_Location(transfo)
 
 
-def reduce_shape(shape: TopoDS_Shape):
+def reduce_shape(shape: TopoDS_Shape) -> tuple:
     """Special function used by pickle to serialize or deserialize OCP Shapes objects"""
     return (deserialize_shape, (serialize_shape(shape),))
 
 
-def reduce_location(location: TopLoc_Location):
+def reduce_location(location: TopLoc_Location) -> tuple:
     """Special function used by pickle to serialize or deserialize OCP Location objects"""
     return (deserialize_location, (serialize_location(location),))
 
