80a5729a5ad2a4d11ce6716f2106e5ac8854c47c
MatthiasJ <>
2023-10-28 14:45:38 -0600

    Add non-planar curve to filter_by(Plane) tests

--- tests/test_build_common.py
+++ tests/test_build_common.py
@@ -365,6 +365,15 @@ class TestShapeList(unittest.TestCase):
                     self.assertAlmostEqual(abs((faces[1].center()-faces[0].center()).dot(plane.z_dir)), 1)
                     axis_index = list(map(int, map(abs, plane.z_dir))).index(1)
                     self.assertTrue(all(abs(list(e.center())[axis_index]) > 0.1 for e in edges))
+                    if plane == Plane.XY:
+                        with BuildLine():
+                            line = Line((0, 0, 0), (10, 0, 0)).edge()
+                            bezier2d = Bezier((0, 0, 0), (5, 3, 0),  (10, 0, 0)).edge()
+                            bezier3d = Bezier((0, 0, 0), (3, 3, 0), (7, 1, 3), (10, 0, 0)).edge()
+                        edges = ShapeList([line, bezier2d, bezier3d]) | plane
+                        self.assertIn(line, edges)
+                        self.assertIn(bezier2d, edges)
+                        self.assertNotIn(bezier3d, edges)
 
         # test filter by type
         with BuildPart() as test:
