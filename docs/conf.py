a15d855db78b1a8cc37d5d20bea74fb8e0d89c1a
marked23 <@marked23>
2025-08-17 12:36:43 -0700

    add sphinx-llms-txt to documentation extensions and dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,7 @@ extensions = [
     "sphinx.ext.graphviz",
     "sphinx.ext.inheritance_diagram",
     "sphinx.ext.viewcode",
+    "sphinx_llms_txt",
     "sphinx_design",
     "sphinx_copybutton",
     "hoverxref.extension",
