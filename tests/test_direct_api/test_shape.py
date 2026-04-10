6cc2d8040eb8f4499d420b964e109914467e197a
Bernhard <b_walter@arcor.de>
2026-04-10 12:38:50 +0200

    add compute_mass tests

--- tests/test_direct_api/test_shape.py
+++ tests/test_direct_api/test_shape.py
@@ -82,8 +82,7 @@ class TestShape(unittest.TestCase):
         self.assertAlmostEqual(box_bb.max.Z, 0, 5)
 
     def test_compute_mass(self):
-        with self.assertRaises(NotImplementedError):
-            Shape.compute_mass(Vertex())
+        self.assertEqual(Vertex().compute_mass(), 0)
 
     def test_combined_center(self):
         objs = [Solid.make_box(1, 1, 1, Plane((x, 0, 0))) for x in [-2, 1]]
@@ -565,7 +564,8 @@ class TestShape(unittest.TestCase):
         empty_bbox = empty.bounding_box()
         self.assertEqual(tuple(empty_bbox.size), (0, 0, 0))
         self.assertIs(empty, empty.mirror(Plane.XY))
-        self.assertEqual(Shape.compute_mass(empty), 0)
+        self.assertEqual(empty.compute_mass(), 0)
+        self.assertEqual(empty.compute_volume(), 0)
         self.assertEqual(empty.entities("Face"), [])
         self.assertEqual(empty.area, 0)
         self.assertIs(empty, empty.rotate(Axis.Z, 90))
