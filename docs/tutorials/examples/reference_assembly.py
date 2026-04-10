6464e9a323d690e9e16f3bbffcc304e517db9c28
Jonathan Wagenet <jonathanwagenet@gmail.com>
2026-04-10 14:07:22 -0400

    Add ref assy to static config

--- docs/tutorials/examples/reference_assembly.py
+++ docs/tutorials/examples/reference_assembly.py
@@ -2,7 +2,7 @@ import copy
 from build123d import *
 
 from tools.svg import write_svg, project_shapes
-from tcv_screenshots import save_model
+# from tcv_screenshots import save_model
 
 screw = import_step("M6-1x12-countersunk-screw.step")
 locs = HexLocations(6, 10, 10).local_locations
@@ -11,6 +11,10 @@ screw_references = [copy.copy(screw).locate(loc) for loc in locs]
 reference_assembly = Compound(children=screw_references)
 reference_assembly.color = Color("white")
 
+# Appears to time out the rendering pipeline
 # save_model(reference_assembly, "reference_assembly")
-# write_svg("reference_assembly", project_shapes(reference_assembly, show_hidden=False))
+# This takes a very long time
+layers = project_shapes(reference_assembly, show_hidden=False)
+layers["visible"]["line_weight"] = Export2D.DEFAULT_LINE_WEIGHT
+write_svg("reference_assembly", layers)
 # export_step(reference_assembly, "reference_assembly.step")
