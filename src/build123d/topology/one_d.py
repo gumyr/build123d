216688ab297639d30bfc8628b20c2328871632c0
javimixet <javibato@hotmail.com>
2026-05-04 19:25:57 +0200

    fix issue 1296
    Addresses [#1296](https://github.com/gumyr/build123d/issues/1296) by reducing radius by 1e-11 to avoid ChFi2dFilletAlgo failure when split vertex equals adjacent edge vertex.

--- src/build123d/topology/one_d.py
+++ src/build123d/topology/one_d.py
@@ -308,6 +308,9 @@ def _solve_wire_fillet_corner_chfi2d(
         corner.connected_edges[1].wrapped,
         Plane.XY.wrapped,
     )
+  
+    if not fillet_builder.Perform(radius):
+        radius -= 1e-11
 
     vertex_point = BRep_Tool.Pnt_s(corner.vertex.wrapped)
     if (
