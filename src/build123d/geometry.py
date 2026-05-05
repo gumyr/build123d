30318f53e7c932c2caf4f3732060dbdeb813b3b6
Daniel Weidmann <weidmann.da@gmail.com>
2026-05-05 22:38:29 +0200

    Remove ordering from Rotation copy coonstructor

--- src/build123d/geometry.py
+++ src/build123d/geometry.py
@@ -2380,7 +2380,6 @@ class Rotation(Location):
     def __init__(
         self,
         rotation: RotationLike,
-        ordering: Extrinsic | Intrinsic == Intrinsic.XYZ,  # type: ignore[valid-type]
     ):
         """Rotation from other RotationLike object."""
 
