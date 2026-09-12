cc71fe3e78e65cfe75eee3dc0cca984e2ac09a36
Bernhard <b_walter@arcor.de>
2026-09-12 12:28:12 +0200

    BRepTopAdaptor_FClass2d dropped from OCP8, replace with IntTools_FClass2d

--- src/build123d/topology/zero_d.py
+++ src/build123d/topology/zero_d.py
@@ -64,7 +64,7 @@ from OCP.BRep import BRep_Tool
 from OCP.BRepAdaptor import BRepAdaptor_Curve2d
 from OCP.BRepBuilderAPI import BRepBuilderAPI_MakeVertex
 from OCP.BRepTools import BRepTools
-from OCP.BRepTopAdaptor import BRepTopAdaptor_FClass2d
+from OCP.IntTools import IntTools_FClass2d
 from OCP.TopExp import TopExp, TopExp_Explorer
 from OCP.collections import IndexedDataMap_TopoDS_Shape_List_TopoDS_Shape_TopTools_ShapeMapHasher
 from OCP.TopoDS import TopoDS, TopoDS_Face, TopoDS_Vertex, TopoDS_Edge
@@ -252,7 +252,7 @@ class Vertex(Shape[TopoDS_Vertex]):
         step = 1e-4 * ((u_max - u_min) ** 2 + (v_max - v_min) ** 2) ** 0.5 / span
         here = BRep_Tool.Parameters_s(self.wrapped, face)
         probe = gp_Pnt2d(here.X() + bisector[0] * step, here.Y() + bisector[1] * step)
-        inside = BRepTopAdaptor_FClass2d(face, TOLERANCE).Perform(probe) == ta.TopAbs_IN
+        inside = IntTools_FClass2d(face, TOLERANCE).Perform(probe) == ta.TopAbs_IN
         return Convexity.CONVEX if inside else Convexity.CONCAVE
 
     def _crease_convexity(self) -> Convexity:
