da64aae3a66711ef593ca217f1cdde557f6ec39a
snoyer <noyer.stephane@gmail.com>
2026-09-25 11:00:09 +0400

    minor type hints

--- src/build123d/text.py
+++ src/build123d/text.py
@@ -138,7 +138,7 @@ class FontManager:
         return self.manager.FindFont(TCollection_AsciiString(name), FONT_ASPECT[style])
 
     def register_font(
-        self, path: str, override: bool = False, single_stroke=False
+        self, path: str, override: bool = False, single_stroke: bool = False
     ) -> list[str]:
         """Register all font faces in a font file and return font face names."""
         _, ext = os.path.splitext(path)
@@ -171,11 +171,11 @@ class FontManager:
         return font_faces
 
     def register_folder(
-        self, path: str, override: bool = False, single_stroke=False
+        self, path: str, override: bool = False, single_stroke: bool = False
     ) -> list[str]:
         """Register all fonts in a folder"""
         exts = ["ttf", "otf", "ttc"]
-        font_faces = []
+        font_faces: list[str] = []
         for ext in exts:
             search = os.path.join(os.path.normpath(path), "*" + ext)
             results = glob.glob(search)
